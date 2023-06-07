70168c733447e963c475cbca0b298a1c5de95081
Rasmus Nørskov <rasmus@norskov.org>
2023-06-07 09:30:24 +0200

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = {
+  plugins: ["import", "unused-imports"],
+  extends: ["plugin:import/recommended", "plugin:import/typescript"],
+  rules: {
+    "sort-imports": [
+      "warn",
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+        allowSeparatedGroups: true,
+      },
+    ],
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling"],
+          "index",
+        ],
+        "newlines-between": "always",
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
+    "unused-imports/no-unused-imports": "error",
+  },
+  settings: { "import/resolver": { typescript: {} } },
+};
